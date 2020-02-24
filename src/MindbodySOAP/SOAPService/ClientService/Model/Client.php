7b2e1c38071b457859a176b98ceadedf4c9f2d2b
Miguel Alcaino <miguel.alcaino@ogb.cl>
2020-02-25 00:23:28 +0100

    Add Client fields
    - AddressLine1
    - AddressLine2
    - City
    - State
    - PostalCode
    - Country
    - MobilePhone
    - BirthDate
    - Gender

--- src/MindbodySOAP/SOAPService/ClientService/Model/Client.php
+++ src/MindbodySOAP/SOAPService/ClientService/Model/Client.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MiguelAlcaino\MindbodyApiClient\MindbodySOAP\SOAPService\ClientService\Model;
 
+use DateTimeImmutable;
 use JMS\Serializer\Annotation as Serializer;
 
 class Client
@@ -52,6 +53,87 @@ class Client
      */
     private $email;
 
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("AddressLine1")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $addressLine1;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("AddressLine2")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $addressLine2;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("City")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $city;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("State")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $state;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("PostalCode")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $postalCode;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("Country")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $country;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("MobilePhone")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $mobilePhone;
+
+    /**
+     * @var DateTimeImmutable|null
+     * @Serializer\SerializedName("BirthDate")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("DateTimeImmutable<'Y-m-d\TH:i:s'>")
+     */
+    private $birthday;
+
+    /**
+     * @var string|null
+     * @Serializer\SerializedName("Gender")
+     * @Serializer\SkipWhenEmpty()
+     * @Serializer\XmlElement(cdata=false)
+     * @Serializer\Type("string")
+     */
+    private $gender;
+
     public function __construct(string $id)
     {
         $this->id = $id;
@@ -67,7 +149,7 @@ class Client
         return $this->promotionalEmailOptIn;
     }
 
-    public function setPromotionalEmailOptIn(?bool $promotionalEmailOptIn): Client
+    public function setPromotionalEmailOptIn(?bool $promotionalEmailOptIn): self
     {
         $this->promotionalEmailOptIn = $promotionalEmailOptIn;
 
@@ -79,7 +161,7 @@ class Client
         return $this->firstName;
     }
 
-    public function setFirstName(?string $firstName): Client
+    public function setFirstName(?string $firstName): self
     {
         $this->firstName = $firstName;
 
@@ -91,7 +173,7 @@ class Client
         return $this->lastName;
     }
 
-    public function setLastName(?string $lastName): Client
+    public function setLastName(?string $lastName): self
     {
         $this->lastName = $lastName;
 
@@ -103,9 +185,117 @@ class Client
         return $this->email;
     }
 
-    public function setEmail(?string $email): Client
+    public function setEmail(?string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddressLine1(): ?string
+    {
+        return $this->addressLine1;
+    }
+
+    public function setAddressLine1(?string $addressLine1): self
+    {
+        $this->addressLine1 = $addressLine1;
+
+        return $this;
+    }
+
+    public function getAddressLine2(): ?string
+    {
+        return $this->addressLine2;
+    }
+
+    public function setAddressLine2(?string $addressLine2): self
+    {
+        $this->addressLine2 = $addressLine2;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(?string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(?string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getMobilePhone(): ?string
+    {
+        return $this->mobilePhone;
+    }
+
+    public function setMobilePhone(?string $mobilePhone): self
+    {
+        $this->mobilePhone = $mobilePhone;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?DateTimeImmutable
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(?DateTimeImmutable $birthday): self
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
 
         return $this;
     }

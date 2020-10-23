602a598759856718a3df1f13e092e7d7cfbb4830
Miguel Alcaino <miguel.alcaino@ogb.cl>
2020-10-23 17:11:38 +0200

    Use $_SERVER instead of $_ENV

--- tests/unit/MindbodySOAP/Helper/MindbodyUtilsTrait.php
+++ tests/unit/MindbodySOAP/Helper/MindbodyUtilsTrait.php
@@ -12,11 +12,11 @@ trait MindbodyUtilsTrait
 {
     public function getMindbodySerializer(bool $useFreeClient = true)
     {
-        $siteIds        = $useFreeClient ? json_decode($_ENV['MINDBODY_FREE_API_SITE_IDS']) : json_decode($_ENV['MINDBODY_SITE_IDS']);
-        $sourceName     = $useFreeClient ? $_ENV['MINDBODY_FREE_API_SOURCE_NAME'] : $_ENV['MINDBODY_SOURCE_NAME'];
-        $sourcePassword = $useFreeClient ? $_ENV['MINDBODY_FREE_API_SOURCE_PASSWORD'] : $_ENV['MINDBODY_SOURCE_PASSWORD'];
-        $adminUser      = $useFreeClient ? $_ENV['MINDBODY_FREE_API_ADMIN_USER'] : $_ENV['MINDBODY_ADMIN_USER'];
-        $adminPassword  = $useFreeClient ? $_ENV['MINDBODY_FREE_API_ADMIN_PASSWORD'] : $_ENV['MINDBODY_ADMIN_PASSWORD'];
+        $siteIds        = $useFreeClient ? json_decode($_SERVER['MINDBODY_FREE_API_SITE_IDS']) : json_decode($_SERVER['MINDBODY_SITE_IDS']);
+        $sourceName     = $useFreeClient ? $_SERVER['MINDBODY_FREE_API_SOURCE_NAME'] : $_SERVER['MINDBODY_SOURCE_NAME'];
+        $sourcePassword = $useFreeClient ? $_SERVER['MINDBODY_FREE_API_SOURCE_PASSWORD'] : $_SERVER['MINDBODY_SOURCE_PASSWORD'];
+        $adminUser      = $useFreeClient ? $_SERVER['MINDBODY_FREE_API_ADMIN_USER'] : $_SERVER['MINDBODY_ADMIN_USER'];
+        $adminPassword  = $useFreeClient ? $_SERVER['MINDBODY_FREE_API_ADMIN_PASSWORD'] : $_SERVER['MINDBODY_ADMIN_PASSWORD'];
 
         $jmsSerializerFactory = new JmsSerializerFactory();
         $jmsSerializer        = $jmsSerializerFactory->create();
@@ -38,26 +38,26 @@ trait MindbodyUtilsTrait
 
     private function getTestUsername(): string
     {
-        return $_ENV['MINDBODY_TEST_CLIENT_EMAIL'];
+        return $_SERVER['MINDBODY_TEST_CLIENT_EMAIL'];
     }
 
     private function getTestUserPassword(): string
     {
-        return $_ENV['MINDBODY_TEST_CLIENT_PASSWORD'];
+        return $_SERVER['MINDBODY_TEST_CLIENT_PASSWORD'];
     }
 
     private function getTestUserId(): string
     {
-        return $_ENV['MINDBODY_TEST_CLIENT_ID'];
+        return $_SERVER['MINDBODY_TEST_CLIENT_ID'];
     }
 
     private function getClientWithPurchasesId(): string
     {
-        return $_ENV['MINDBODY_TEST_USER_WITH_PURCHASES_ID'];
+        return $_SERVER['MINDBODY_TEST_USER_WITH_PURCHASES_ID'];
     }
 
     private function getPromoCode(): string
     {
-        return $_ENV['MINDBODY_TEST_PROMOCODE'];
+        return $_SERVER['MINDBODY_TEST_PROMOCODE'];
     }
 }

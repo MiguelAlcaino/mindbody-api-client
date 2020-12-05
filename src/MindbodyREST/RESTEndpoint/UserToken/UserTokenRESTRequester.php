13272fd44f87f3895a22e55de7b2b8211ed987a5
Miguel Alcaino <miguel.alcaino@ogb.cl>
2020-12-05 16:26:04 +0100

    Add Exception when AccessDenied in issueUserToken

--- src/MindbodyREST/RESTEndpoint/UserToken/UserTokenRESTRequester.php
+++ src/MindbodyREST/RESTEndpoint/UserToken/UserTokenRESTRequester.php
@@ -2,21 +2,31 @@
 
 namespace MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\UserToken;
 
+use GuzzleHttp\Exception\ClientException;
 use MiguelAlcaino\MindbodyApiClient\MindbodyREST\BaseRequester\RESTRequesterExecutor;
+use MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\Common\Util\ResponseExceptionHandler;
 use MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\UserToken\Request\POSTIssueRequest;
 use MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\UserToken\Response\POSTIssueResponse;
 
 class UserTokenRESTRequester
 {
-    private RESTRequesterExecutor $restRequester;
+    private RESTRequesterExecutor    $restRequester;
+    private ResponseExceptionHandler $responseExceptionHandler;
 
-    public function __construct(RESTRequesterExecutor $restRequester)
-    {
-        $this->restRequester = $restRequester;
+    public function __construct(
+        RESTRequesterExecutor $restRequester,
+        ResponseExceptionHandler $responseExceptionHandler
+    ) {
+        $this->restRequester            = $restRequester;
+        $this->responseExceptionHandler = $responseExceptionHandler;
     }
 
     public function issueUserToken(POSTIssueRequest $request): POSTIssueResponse
     {
-        return $this->restRequester->executeRequest($request, POSTIssueResponse::class);
+        try {
+            return $this->restRequester->executeRequest($request, POSTIssueResponse::class);
+        } catch (ClientException $exception) {
+            throw $this->responseExceptionHandler->getMindbodyException($exception);
+        }
     }
 }

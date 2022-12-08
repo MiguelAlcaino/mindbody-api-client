10923c39775c3181fce8d48914a20422ff233a23
Miguel Alcaino <miguel.alcaino@ogb.cl>
2022-12-08 14:21:48 -0300

    Add missing fields to ClientService
    Also add startDate and endDate to

--- src/MindbodyREST/RESTEndpoint/Client/Request/GETClientServicesRequest.php
+++ src/MindbodyREST/RESTEndpoint/Client/Request/GETClientServicesRequest.php
@@ -2,6 +2,7 @@
 
 namespace MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\Client\Request;
 
+use DateTimeImmutable;
 use JMS\Serializer\Annotation as Serializer;
 use MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\Common\Model\RESTRequest;
 use MiguelAlcaino\MindbodyApiClient\MindbodyREST\RESTEndpoint\Common\Model\UserStaffTokenRequiredInterface;
@@ -15,10 +16,34 @@ class GETClientServicesRequest extends RESTRequest implements UserStaffTokenRequ
      * @Serializer\SerializedName("ClientId")
      */
     private string $clientId;
+    /**
+     * @Serializer\SerializedName("StartDate")
+     * @Serializer\Type("DateTimeImmutable<'Y-m-d'>")
+     */
+    private DateTimeImmutable $startDate;
+    /**
+     * @Serializer\SerializedName("EndDate")
+     * @Serializer\Type("DateTimeImmutable<'Y-m-d'>")
+     */
+    private DateTimeImmutable $endDate;
 
     public function __construct(string $clientId)
     {
         $this->clientId = $clientId;
+    }
+
+    public function setStartDate(DateTimeImmutable $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function setEndDate(DateTimeImmutable $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
     }
 
     public function getMethod(): string
@@ -30,5 +55,4 @@ class GETClientServicesRequest extends RESTRequest implements UserStaffTokenRequ
     {
         return 'client/clientservices';
     }
-
 }
--- src/MindbodyREST/RESTEndpoint/Client/Response/Model/ClientService.php
+++ src/MindbodyREST/RESTEndpoint/Client/Response/Model/ClientService.php
@@ -8,18 +8,49 @@ use JMS\Serializer\Annotation as Serializer;
 class ClientService
 {
     /**
+     * @Serializer\SerializedName("Id")
+     */
+    private int $id;
+    /**
      * @var DateTimeImmutable
      * @Serializer\SerializedName("ActiveDate")
      * @Serializer\Type("DateTimeImmutable<'Y-m-d\TH:i:s'>")
      */
     private DateTimeImmutable $activeDate;
-
     /**
      * @var DateTimeImmutable
      * @Serializer\SerializedName("ExpirationDate")
      * @Serializer\Type("DateTimeImmutable<'Y-m-d\TH:i:s'>")
      */
     private DateTimeImmutable $expirationDate;
+    /**
+     * @Serializer\SerializedName("Current")
+     */
+    private bool $current;
+    /**
+     * @Serializer\SerializedName("Name")
+     */
+    private string $name;
+    /**
+     * @Serializer\SerializedName("PaymentDate")
+     */
+    private string $paymentDate;
+    /**
+     * @Serializer\SerializedName("Count")
+     */
+    private int $count;
+    /**
+     * @Serializer\SerializedName("Remaining")
+     */
+    private int $remaining;
+    /**
+     * @Serializer\SerializedName("SiteId")
+     */
+    private int $siteId;
+    /**
+     * @Serializer\SerializedName("ProductId")
+     */
+    private int $productId;
 
     public function getActiveDate(): DateTimeImmutable
     {
@@ -29,5 +60,45 @@ class ClientService
     public function getExpirationDate(): DateTimeImmutable
     {
         return $this->expirationDate;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function isCurrent(): bool
+    {
+        return $this->current;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPaymentDate(): DateTimeImmutable
+    {
+        return new DateTimeImmutable($this->paymentDate);
+    }
+
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function getRemaining(): int
+    {
+        return $this->remaining;
+    }
+
+    public function getSiteId(): int
+    {
+        return $this->siteId;
+    }
+
+    public function getProductId(): int
+    {
+        return $this->productId;
     }
 }
